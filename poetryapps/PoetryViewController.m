e27ca6103b7a1aed752442db71b49703211fbf91
kevin yang <fdxyang@gmail.com>
2013-11-26 13:55:50 +0800

    add some functions, open file and display content from file

--- poetryapps/PoetryViewController.m
+++ poetryapps/PoetryViewController.m
@@ -19,33 +19,25 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     
+    CGRect screenRect = [[UIScreen mainScreen] bounds];
+    
     NSBundle *mainBundle = [NSBundle mainBundle];
-    //NSString *dataString = [mainBundle pathForResource:@"data" ofType:@"strings"];
-    
-    //NSLog(@"data string = %@",dataString);
-    
     NSString *txtPath = [mainBundle pathForResource:@"data" ofType:@"txt"];
     
     NSString *string = [[NSString  alloc] initWithContentsOfFile:txtPath encoding:NSUTF8StringEncoding error:nil];
     NSLog(@"%@",string);
-    CGRect labelframe = CGRectMake(10, 10, 300, 480);
+    CGRect labelframe = CGRectMake(0, 0, screenRect.size.width, screenRect.size.height);
     UILabel *label = [[UILabel alloc] initWithFrame:labelframe];
     //    将sring内容赋给lable的text属性
     label.text=string;
     //  背景颜色设置成透明色
-    label.backgroundColor = [UIColor clearColor];
+    label.backgroundColor = [UIColor blackColor];
     //    字体颜色设置成红色
-    label.textColor= [UIColor redColor];
-    //    设置lable文字左对齐显示
-    //label.textAlignment = UITextAlignmentLeft;
-    //    自动这行设置
-    //label.lineBreakMode = UILineBreakModeCharacterWrap;
+    label.textColor= [UIColor whiteColor];
     //等于0表示可根据具实际情况自动变动
     label.numberOfLines = 0;
     
     [self.view addSubview:label];
-    
-    
 }
 
 - (void)didReceiveMemoryWarning
