a5b133e2111549a017d3c76c09c52cf1bd14f44c
kevin yang <fdxyang@gmail.com>
2013-11-26 11:46:29 +0800

    change branch poetryapps_development

--- poetryapps/PoetryViewController.m
+++ poetryapps/PoetryViewController.m
@@ -18,6 +18,34 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    NSBundle *mainBundle = [NSBundle mainBundle];
+    //NSString *dataString = [mainBundle pathForResource:@"data" ofType:@"strings"];
+    
+    //NSLog(@"data string = %@",dataString);
+    
+    NSString *txtPath = [mainBundle pathForResource:@"data" ofType:@"txt"];
+    
+    NSString *string = [[NSString  alloc] initWithContentsOfFile:txtPath encoding:NSUTF8StringEncoding error:nil];
+    NSLog(@"%@",string);
+    CGRect labelframe = CGRectMake(10, 10, 300, 480);
+    UILabel *label = [[UILabel alloc] initWithFrame:labelframe];
+    //    将sring内容赋给lable的text属性
+    label.text=string;
+    //  背景颜色设置成透明色
+    label.backgroundColor = [UIColor clearColor];
+    //    字体颜色设置成红色
+    label.textColor= [UIColor redColor];
+    //    设置lable文字左对齐显示
+    //label.textAlignment = UITextAlignmentLeft;
+    //    自动这行设置
+    //label.lineBreakMode = UILineBreakModeCharacterWrap;
+    //等于0表示可根据具实际情况自动变动
+    label.numberOfLines = 0;
+    
+    [self.view addSubview:label];
+    
+    
 }
 
 - (void)didReceiveMemoryWarning
