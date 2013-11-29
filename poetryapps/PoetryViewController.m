98d861f6a13fd4e06a448194e91b8258862bbfad
kevin yang <fdxyang@gmail.com>
2013-11-29 14:50:36 +0800

    add storyboard for reading, goto, search , and setting tab bar

--- poetryapps/PoetryViewController.m
+++ poetryapps/PoetryViewController.m
@@ -20,6 +20,7 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     
+    /*
     CGRect screenRect = [[UIScreen mainScreen] bounds];
     
     NSBundle *mainBundle = [NSBundle mainBundle];
@@ -32,16 +33,17 @@
     //    将sring内容赋给lable的text属性
     label.text=string;
     //  背景颜色设置成透明色
-    label.backgroundColor = [UIColor blackColor];
+    label.backgroundColor = [UIColor whiteColor];
     //    字体颜色设置成红色
-    label.textColor= [UIColor whiteColor];
+    label.textColor= [UIColor blackColor];
+    
     //等于0表示可根据具实际情况自动变动
     label.numberOfLines = 0;
     
     [self.view addSubview:label];
     
-    
-    
+    */
+    /*
     
     // [CASPER] 2013.11.26 Sample code for saving into core data
     PoetryCoreData *PoetryDataBase = [[PoetryCoreData alloc] init];
@@ -58,7 +60,7 @@
     NSLog(@"Poetry Name = %@", [[PoetryList firstObject] valueForKey:POETRY_CORE_DATA_NAME_KEY]);
     NSLog(@"Poetry Content = %@", [[PoetryList firstObject] valueForKey:POETRY_CORE_DATA_CONTENT_KEY]);
     // [CASPER] 2013.11.26 Sample code for fetching core data ==
-
+*/
     
 }
 
