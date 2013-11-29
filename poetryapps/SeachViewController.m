f516f9a43409fe999c3a0c993873033fa4ef5315
GIGIGUN <sprewell20051116@gmail.com>
2013-11-29 16:37:21 +0800

    Add navigation Table view and Storyboard ID for each view

--- poetryapps/SeachViewController.m
+++ poetryapps/SeachViewController.m
@@ -31,7 +31,7 @@
     _HistoryData = [_PoetryDatabase Poetry_CoreDataFetchDataInHistory];
 
     // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
+    self.clearsSelectionOnViewWillAppear = YES;
  
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
