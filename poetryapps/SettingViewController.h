87aca689b1c9a66bc1dbcd7542fa895227ddeb38
GIGIGUN <sprewell20051116@gmail.com>
2013-11-30 15:54:34 +0800

    Add Setting core data attribute and SettingViewController

---
+++ poetryapps/SettingViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingViewController.h
+//  poetryapps
+//
+//  Created by GIGIGUN on 2013/11/30.
+//  Copyright (c) 2013年 cc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingViewController : UIViewController
+
+@end
