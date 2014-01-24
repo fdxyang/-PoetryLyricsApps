e94dd28824cd7a0676d144e4ee930938514dab5f
kevin yang <fdxyang@gmail.com>
2014-01-24 11:39:12 +0800

    modify about me page

--- poetryapps/AboutMeViewController.m
+++ poetryapps/AboutMeViewController.m
@@ -37,45 +37,50 @@
     
     CGRect Frame;
     if (IS_IPHONE5) {
-        Frame = CGRectMake(0, 0, UI_SCREEN_WIDTH, UI_SCREEN_4_INCH_HEIGHT);
-        contactPos = UI_SCREEN_4_INCH_HEIGHT-100;
+        Frame = CGRectMake(0, 65, UI_SCREEN_WIDTH, UI_SCREEN_4_INCH_HEIGHT-115);
+        contactPos = UI_SCREEN_4_INCH_HEIGHT-115-100;
     } else {
-        Frame = CGRectMake(0, 0, UI_SCREEN_WIDTH, UI_SCREEN_3_5_INCH_HEIGHT);
-        contactPos = UI_SCREEN_3_5_INCH_HEIGHT-100;
+        Frame = CGRectMake(0, 65, UI_SCREEN_WIDTH, UI_SCREEN_3_5_INCH_HEIGHT-115);
+        contactPos = UI_SCREEN_3_5_INCH_HEIGHT-115-60;
     }
     [_Scroller setFrame:Frame];
     [self.view addSubview:_Scroller];
 
-    CGSize Size = CGSizeMake(UI_SCREEN_WIDTH, 2000.0f);
+    //CGSize Size = CGSizeMake(UI_SCREEN_WIDTH, 500.0f);
+    CGSize Size = CGSizeMake(UI_SCREEN_WIDTH, 300.0f);
     
     [_Scroller setContentSize:Size];
     
     float teamlogo_x = 70.0;
-    float teamlogo_y = 80.0;
+    float teamlogo_y = 0.0;
     float teamlogo_width = 180.0;
-    float teamlogo_height = 135.0;
+    float teamlogo_height = 180.0;
 
     //logo
     UIImageView *teamLogo = [[UIImageView alloc] initWithFrame:CGRectMake(teamlogo_x, teamlogo_y, teamlogo_width, teamlogo_height)];
     [teamLogo setImage:[UIImage imageNamed:@"teamlogo.png"]];
     [_Scroller addSubview:teamLogo];
     
-    UILabel *teamName = [[UILabel alloc]initWithFrame:CGRectMake(teamlogo_x+10, teamlogo_y+teamlogo_height,320, 60)];
+    UILabel *teamName = [[UILabel alloc]initWithFrame:CGRectMake(0, teamlogo_y+teamlogo_height,320, 50)]; // (0,180,320,50)
     [teamName setFont:[UIFont fontWithName:@"HelveticaNeue-Light" size:30.0]];
     [teamName setText:@"Hippos Color"];
+    teamName.textAlignment = NSTextAlignmentCenter;
     [_Scroller addSubview:teamName];
     
     
-    UILabel *teamYear = [[UILabel alloc]initWithFrame:CGRectMake(teamlogo_x+10+10, teamlogo_y+teamlogo_height+5+30+5, teamlogo_width-20-20, 30)];
+    UILabel *teamYear = [[UILabel alloc]initWithFrame:CGRectMake(0, teamlogo_y+teamlogo_height+50, 320, 30)]; // (0,230,320,30)
     [teamYear setFont:[UIFont fontWithName:@"HelveticaNeue-Light" size:15.0]];
     teamYear.textColor = [UIColor grayColor];
     [teamYear setText:@"Since 2014"];
+    teamYear.textAlignment = NSTextAlignmentCenter;
     [_Scroller addSubview:teamYear];
     
-    UILabel *teamContact = [[UILabel alloc]initWithFrame:CGRectMake(5, contactPos, 310, 30)];
-    [teamContact setFont:[UIFont fontWithName:@"HelveticaNeue-Light" size:15.0]];
+    
+    UILabel *teamContact = [[UILabel alloc]initWithFrame:CGRectMake(0, contactPos, 320, 40)];
+    [teamContact setFont:[UIFont fontWithName:@"HelveticaNeue-Light" size:18.0]];
     teamContact.backgroundColor = [UIColor grayColor];
     teamContact.textColor = [UIColor whiteColor];
+    teamContact.textAlignment = NSTextAlignmentCenter;
     [teamContact setText:@"聯絡我們：hipposcolor@gmail.com"];
     [_Scroller addSubview:teamContact];
 }
